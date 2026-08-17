a3801ac8105f5019f7022323c769eddebebfa655
Dina Emad <ttwta7138@gmail.com>
2026-08-18 00:30:56 +0300

    Add basic calculator functionality in Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+print('| Welcome to Calculator App |')
+first_number = int(input("Enter first number : "))
+second_number = int(input("Enter second number : "))
+print('| -------------------------------- |')
+Add = first_number + second_number
+Subtract = first_number - second_number
+Multiply = first_number * second_number
+Divide = first_number / second_number
+Modulus = first_number % second_number
+print("Adding : " + str(Add))
+print("Subtract : " + str(Subtract))
+print("Multiply : " + str(Multiply))
+print("Divide : " + str(Divide))
+print("Modulus : " + str(Modulus))
