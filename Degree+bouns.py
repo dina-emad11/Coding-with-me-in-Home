398690ccda8e3d6ac61edfb1f5c3cad903eae2e5
Dina Emad <ttwta7138@gmail.com>
2026-08-19 10:23:59 +0300

    Create Degree+bouns.py

---
+++ Degree+bouns.py
@@ -0,0 +1,7 @@
+grades = [75,82,60,49,90,28]
+new_grades = []
+for gra in grades:
+    bouns = gra+5
+    new_grades.append(bouns)
+print("Original Grades: ",grades)
+print("Grade After bouns: ",new_grades)
